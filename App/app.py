4f4c91d2ec796382c995d16af94a7024234909a7
Hariom Singh <hariomsingh0398@gmail.com>
2024-01-22 09:57:25 +0530

    xss in flask app form

--- App/app.py
+++ App/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route('/About-Me')
+@app.route('/About-Me', methods=['GET','POST'] )
 def about():
-    return render_template("about_me.html")
+    user_input = ''
+    if request.method=='POST':
+        user_input = request.form.get('xss_input','')
+    return render_template("about_me.html", user_input= user_input)
 
 if __name__ == '__main__':
     app.run(debug=True)
